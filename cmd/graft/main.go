ca9d98bac15bbe70d492cbc05b395dc9c3315d75
Shahariar Munir <skssmd78475@gmail.com>
2026-08-26 09:11:45 +0100

    Merge branch 'main' of https://github.com/The-Graft-Project/Graft

--- cmd/graft/main.go
+++ cmd/graft/main.go
@@ -22,7 +22,7 @@ func main() {
 	if len(args) > 0 {
 		arg := args[0]
 		if arg == "-v" || arg == "--version" {
-			fmt.Println("v2.5.6")
+			fmt.Println("v2.5.7")
 			return
 		}
 		if arg == "--help" {
